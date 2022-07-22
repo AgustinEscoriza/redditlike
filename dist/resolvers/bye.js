4af77ac8da55090596f11692fddd73e1e1a9e85d
unknown <agusescoriza@outlook.es>
2022-07-22 20:50:34 -0300

    setup graphql, express

---
+++ dist/resolvers/bye.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=bye.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello world")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConf from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+// import { Post } from "./entities/Post";
+
+const main = async () => {
+    const app = express();
+    const orm = await MikroORM.init(microConf);
+    await orm.getMigrator().up();
+    // const emFork = orm.em.fork();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app});
+    app.listen(4000, ()=>{
+        console.log('server started on localhost:4000')
+    });
+};
+
+main().catch((err)=>{
+    console.error(err);
+});
